9a7b5dc4a7d6599b146a2ae31f1d75b85b2613f7
Michiel Borkent <michielborkent@gmail.com>
2021-09-09 23:21:17 +0200

    Make it work

--- src/clj_easy/graal_build_time.clj
+++ src/clj_easy/graal_build_time.clj
@@ -1,19 +1,46 @@
 (ns clj-easy.graal-build-time
   (:require [clojure.string :as str])
+  (:import [java.nio.file Path]
+           [java.util.jar JarFile JarFile$JarFileEntry])
   (:gen-class
-   :methods [^{:static true} [packageList [] "[Ljava.lang.String;"]
+   :methods [^{:static true} [packageList [java.util.List] "[Ljava.lang.String;"]
              ^{:static true} [packageListStr ["[Ljava.lang.String;"] String]]))
 
-(defn -packageList []
-  (->> (map ns-name (all-ns))
-       (remove #(str/starts-with? % "clojure"))
-       (map #(str/split (str %) #"\."))
-       (keep butlast)
-       (map #(str/join "." %))
-       distinct
-       (map munge)
-       (cons "clojure")
-       into-array))
+(defn keep-fn [nm]
+  (when (and
+         (not (str/starts-with? nm "clojure"))
+         (str/ends-with? nm "__init.class"))
+    (-> nm
+        (str/replace "__init.class" "")
+        (str/replace "/" "."))))
+
+(defn packages-from-jar
+  [^Path jar-file]
+  (with-open [jar (JarFile. (.toFile jar-file))]
+    (let [entries (enumeration-seq (.entries jar))
+          packages (keep (fn [^JarFile$JarFileEntry x]
+                           (let [nm (.getName x)]
+                             (keep-fn nm))) entries)]
+      (vec packages))))
+
+(defn packages-from-dir [^Path dir]
+  ;; TODO: this needs unit tests as it's not exercises in integration test
+  (let [f (.toFile dir)
+        files (file-seq f)
+        relatives (map (fn [^java.io.File f]
+                         (let [path (.toPath f)]
+                           (.relativize dir path)))
+                       files)
+        names (map str relatives)
+        packages (keep keep-fn names)]
+    packages))
+
+(defn -packageList [paths]
+  (into-array (cons "clojure"
+                    (mapcat (fn [path]
+                              (if (str/ends-with? (str path) ".jar")
+                                (packages-from-jar path)
+                                (packages-from-dir path))) paths))))
 
 (defn -packageListStr [pl]
   (str/join ", " pl))
--- src/clj_easy/graal_build_time/InitAtBuildTimeFeature.java
+++ src/clj_easy/graal_build_time/InitAtBuildTimeFeature.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.nio.file.Path;
+
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
@@ -8,7 +11,8 @@ public class InitAtBuildTimeFeature implements Feature {
 
     @Override
     public void duringSetup(DuringSetupAccess access) {
-        String[] packages = clj_easy.graal_build_time.packageList();
+        List<Path> classPath = access.getApplicationClassPath();
+        String[] packages = clj_easy.graal_build_time.packageList(classPath);
         String packagesStr = clj_easy.graal_build_time.packageListStr(packages);
         System.out.println("Registering packages for build time initialization: " + packagesStr);
         RuntimeClassInitialization.initializeAtBuildTime(packages);
