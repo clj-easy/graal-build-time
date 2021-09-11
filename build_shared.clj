24cb10e81e7d04cc399ba2958d336f722c6fd896
Michiel Borkent <michielborkent@gmail.com>
2021-09-11 22:35:29 +0200

    simplify version

--- build_shared.clj
+++ build_shared.clj
@@ -1,20 +1,10 @@
 (ns build-shared
   (:require [babashka.fs :as fs]
-            [babashka.process :as p]
             [clojure.string :as str]))
-
-(defn git-count-revs
-  [_]
-  (-> (p/process ["git" "rev-list" "HEAD" "--count"]
-                 {:err :inherit
-                  :out :string})
-      (p/check)
-      :out
-      str/trim))
 
 (def target "target")
 (def lib 'com.github.clj-easy/graal-build-time)
-(def version (delay (format "0.0.%s" (git-count-revs nil))))
+(def version (str/trim (slurp "resources/clj-easy/graal-build-time-version.txt")))
 (def jar-file (delay (format "target/%s-%s.jar" (name lib) @version)))
 (def uberjar "target/test.jar")
 (def sources ["src"])
