9bcd75b6057b4592b0be0ebfc981d97f1cc386b7
Michiel Borkent <michielborkent@gmail.com>
2021-09-12 10:04:11 +0200

    Version logic

--- build_shared.clj
+++ build_shared.clj
@@ -2,10 +2,14 @@
   (:require [babashka.fs :as fs]
             [clojure.string :as str]))
 
+(def version-file "resources/clj-easy/graal-build-time-version.txt")
+(def version nil)
+(defn refresh-version []
+  (alter-var-root #'version (constantly (str/trim (slurp version-file)))))
+(refresh-version)
+
 (def target "target")
 (def lib 'com.github.clj-easy/graal-build-time)
-(def version-file "resources/clj-easy/graal-build-time-version.txt")
-(def version (str/trim (slurp version-file)))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 (def uberjar "target/test.jar")
 (def sources ["src"])
