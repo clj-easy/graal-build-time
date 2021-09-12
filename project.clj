5ef7c7a768238b927b7f089f0a4ef562e8717a8d
Michiel Borkent <michielborkent@gmail.com>
2021-09-12 12:42:23 +0200

    Clean up

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject com.github.clj-easy/graal-build-time "0.0.1"
-  :dependencies [[org.graalvm.nativeimage/svm "21.2.0" :scope "provided"]]
-  :aot [clj-easy.graal-build-time]
-  :java-source-paths ["src"]
-  :profiles {:javac {:aot [clj-easy.graal-build-time]}}
-)
