43f6071b40bfe2271ad2aca72b340fa2269e5259
lread <lee@dlread.com>
2021-09-24 10:20:21 -0400

    Add work-around to new build.clj files
    We already had this work-around in our existing build.clj,
    added to new build.clj files under test-hello-world.

--- test-hello-world/build.clj
+++ test-hello-world/build.clj
@@ -1,3 +1,6 @@
+;; see https://ask.clojure.org/index.php/10905/control-transient-deps-that-compiled-assembled-into-uberjar?show=10913#c10913
+(require 'clojure.tools.deps.alpha.util.s3-transporter)
+
 (ns build
   (:refer-clojure :exclude [compile])
   (:require [babashka.fs :as fs]
